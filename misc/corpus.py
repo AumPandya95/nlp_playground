4c615ba7f9017a1de927afc0926f9fa60a56ed4b
Aum Pandya <aumpandya.95@gmail.com>
2021-12-09 18:58:49 +0530

    Chore: Experimentation on data structure type

---
+++ misc/corpus.py
@@ -0,0 +1,27 @@
+
+def main(corpus):
+    cat_corp = corpus.get("categorical_column_objects")
+    num_corp = corpus.get("numerical_column_objects")
+
+    for each_dict in cat_corp:
+        each_dict["name"] = each_dict["name"].lower()
+        each_dict.pop("singular")
+        each_dict.pop("plural")
+    
+    for each_dict in num_corp:
+        each_dict["name"] = each_dict["name"].lower()
+        each_dict.pop("singular")
+        each_dict.pop("plural")
+    
+    return corpus
+
+
+if __name__ == "__main__":
+    import json
+    corp_file = open("misc/corpus.json")
+    corp = json.load(corp_file)
+
+    fin_corpus = main(corp)
+    with open("misc/corpus_transformed.json", "w") as file:
+        json.dump(fin_corpus, file, indent=4)
+        file.close()
